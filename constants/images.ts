d4e200252646693341558b30f3e8c1f24e343aa8
Prince Pal <princepal9120@gmail.com>
2025-05-25 18:33:48 +0000

    refactor: rename Colors constant and restructure color definitions; add spacing and predefined images

---
+++ constants/images.ts
@@ -0,0 +1,42 @@
+export const PREDEFINED_IMAGES = [
+  {
+    id: '1',
+    label: 'Minecraft',
+    url: 'https://images.pexels.com/photos/5410935/pexels-photo-5410935.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+  },
+  {
+    id: '2',
+    label: 'Console Gaming',
+    url: 'https://images.pexels.com/photos/442576/pexels-photo-442576.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+  },
+  {
+    id: '3',
+    label: 'Racing Games',
+    url: 'https://images.pexels.com/photos/3894383/pexels-photo-3894383.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+  },
+  {
+    id: '4',
+    label: 'Mobile Gaming',
+    url: 'https://images.pexels.com/photos/4004374/pexels-photo-4004374.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+  },
+  {
+    id: '5',
+    label: 'VR Gaming',
+    url: 'https://images.pexels.com/photos/8988272/pexels-photo-8988272.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+  },
+  {
+    id: '6',
+    label: 'Retro Gaming',
+    url: 'https://images.pexels.com/photos/2728255/pexels-photo-2728255.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+  },
+  {
+    id: '7',
+    label: 'PC Gaming',
+    url: 'https://images.pexels.com/photos/3165335/pexels-photo-3165335.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+  },
+  {
+    id: '8',
+    label: 'Gaming Setup',
+    url: 'https://images.pexels.com/photos/4792731/pexels-photo-4792731.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+  },
+];
